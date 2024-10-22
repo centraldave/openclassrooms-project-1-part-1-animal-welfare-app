6e11a8800d7dedd2d35c14ad56ce2cf104c3e6bb
David <yellow.book1968@fastmail.com>
2024-10-22 17:00:18 +0200

    feat: animal cells, animal details

--- app/src/main/java/com/animals/safety/screens/AnimalDetailsScreen.kt
+++ app/src/main/java/com/animals/safety/screens/AnimalDetailsScreen.kt
@@ -1,18 +1,35 @@
 package com.animals.safety.screens
 
+import android.graphics.Paint.Align
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.animals.safety.R
 import com.animals.safety.data.Animal
 import com.animals.safety.data.Breed
@@ -57,9 +74,111 @@ fun AnimalDetailsScreen(
 private fun AnimalDetails(
   modifier: Modifier = Modifier,
   animal: Animal,
-)
-{
+) {
   //TODO: A compléter
+  Column (
+    modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally
+  ) {
+    Row (
+      modifier = Modifier.weight(1f)
+    ) {
+        Box (
+          contentAlignment = Alignment.Center,
+          modifier = Modifier
+            .fillMaxWidth()
+        ) {
+          Image(
+            painter = painterResource(id = animal.breed.cover), // Replace with your image resource
+            contentDescription = "Description of the image",
+            modifier = Modifier
+              .fillMaxSize(),
+            contentScale = ContentScale.Crop
+          )
+
+          Text( text = animal.name,
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier
+              .align(Alignment.BottomStart)
+              .padding(16.dp),
+            color = Color.White
+          )
+        }
+    }
+
+    Spacer(modifier = Modifier.height(50.dp))
+
+    Row (
+      modifier = Modifier.weight(1f)
+    ) {
+      Column {
+        Row {
+          Column {
+            AnimalDetailLabel(
+              R.drawable.ic_age,
+              stringResource(R.string.hint_age),
+              "${animal.age} years old"
+            )
+          }
+
+          Column {
+            AnimalDetailLabel(
+              R.drawable.ic_weight,
+              stringResource(R.string.hint_weight),
+              "${animal.weight} (Kg)"
+            )
+          }
+        }
+
+        Row (
+          modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+          Column {
+            AnimalDetailLabel(
+              R.drawable.ic_height,
+              stringResource(R.string.hint_height),
+              "${animal.height} (cm)"
+            )
+          }
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun AnimalDetailLabel(
+  @DrawableRes image: Int,
+  imageDescription: String,
+  labelText: String
+) {
+  Box {
+    Column (
+      modifier = Modifier
+        .padding(16.dp),
+      horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+      Row {
+        Image(
+          painter = painterResource(id = image), // Replace with your image resource
+          contentDescription = imageDescription,
+          modifier = Modifier
+            .height(50.dp),
+          colorFilter = ColorFilter.tint(Color.Black),
+          contentScale = ContentScale.Crop
+        )
+      }
+
+      Row (
+        modifier = Modifier
+          .padding(16.dp)
+      ) {
+        Text( text = labelText,
+          style = MaterialTheme.typography.labelLarge
+        )
+      }
+    }
+  }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/animals/safety/screens/HomeScreen.kt
+++ app/src/main/java/com/animals/safety/screens/HomeScreen.kt
@@ -1,10 +1,18 @@
 package com.animals.safety.screens
 
+import android.graphics.Paint.Align
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -14,11 +22,14 @@ import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -91,14 +102,45 @@ private fun HomeCell(
   onAnimalClick: (Animal) -> Unit,
 ) {
   //TODO: A compléter
+  val aDescription: String = "${animal.breed.name}, ${animal.age} years old! Its weight is ${animal.weight} kilograms and its size is ${animal.height} centimeters."
   Box(
     modifier = Modifier
       .fillMaxWidth()
-      .height(20.dp)
+      .padding(16.dp)
       .clickable {
         onAnimalClick(animal)
       }
-  )
+  ) {
+
+    Row (
+      modifier = Modifier.align(Alignment.CenterStart)
+        .height(128.dp)
+    ) {
+      Column (
+        modifier = Modifier.align(Alignment.CenterVertically)
+      ) {
+        Image(
+          painter = painterResource(id = animal.breed.cover),
+          contentDescription = aDescription,
+          modifier = Modifier.fillMaxHeight()
+        )
+      }
+
+      Spacer(modifier = Modifier.width(16.dp))
+
+      Column {
+        Text(
+          text = animal.name,
+          style = MaterialTheme.typography.titleLarge
+        )
+
+        Text(
+          text = aDescription,
+          modifier = Modifier.padding(top = 16.dp)
+        )
+      }
+    }
+  }
 }
 
 @Preview(showBackground = true)
